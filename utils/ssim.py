cc7e67a031d0bf2ffbaa122de36175ed1edd6c82
Javier FernandezJavier FernandezJavier Fernandez <javier.f3rnand3z@gmail.com>
2022-08-23 19:50:37 +0200

    Code and README added

---
+++ utils/ssim.py
@@ -0,0 +1,33 @@
+import cv2
+
+import numpy as np
+from scipy import signal
+
+
+def ssim(img1, img2, size_window=11):
+    K = [0.01, 0.03]
+    L = 255
+    kernel = cv2.getGaussianKernel(size_window, 1.5)
+    window = kernel * kernel.T
+
+    C1 = (K[0]*L)**2
+    C2 = (K[1]*L)**2
+
+    img1 = np.float64(img1)
+    img2 = np.float64(img2)
+ 
+    mu1 = signal.convolve2d(img1, window, 'valid')
+    mu2 = signal.convolve2d(img2, window, 'valid')
+    
+    mu1_sq = mu1*mu1
+    mu2_sq = mu2*mu2
+    mu1_mu2 = mu1*mu2
+
+    sigma1_sq = signal.convolve2d(img1*img1, window, 'valid') - mu1_sq
+    sigma2_sq = signal.convolve2d(img2*img2, window, 'valid') - mu2_sq
+    sigma12 = signal.convolve2d(img1*img2, window, 'valid') - mu1_mu2
+   
+    ssim_map = ((2*mu1_mu2 + C1)*(2*sigma12 + C2))/((mu1_sq + mu2_sq + C1)*(sigma1_sq + sigma2_sq + C2))
+    mssim = np.mean(ssim_map)
+
+    return mssim, ssim_map
